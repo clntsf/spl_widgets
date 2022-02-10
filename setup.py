bd03c6eeeaa5c0f0b4994e6106d0eb6f57f2f35d
ctsf1 <colin.tsf@gmail.com>
2022-02-10 16:30:14 +0000

    fixed stuff

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="spl_widgets",
-    version="0.2.6",
+    version="0.2.8",
     author="Colin Simon-Fellowes",
     author_email="colin.tsf@gmail.com",
     description="Widgets for the Barnard Speech Perception Laboratory",
