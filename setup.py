4c1687804bfde8a5fa0d1799e56ca3c16d002a14
ctsf1 <colin.tsf@gmail.com>
2022-12-12 13:50:12 +0000

    version change: 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="spl_widgets",
-    version="0.7.0",
+    version="0.7.1",
     author="Colin Simon-Fellowes",
     author_email="colin.tsf@gmail.com",
     description="Widgets for the Barnard Speech Perception Laboratory",
